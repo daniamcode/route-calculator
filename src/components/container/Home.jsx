3598023d70fd69da4aa01f58ac06b937a4290d96
Dani Alcala <daniamcode@gmail.com>
2020-12-12 00:19:37 +0100

    add first version of the map

--- src/components/container/Home.jsx
+++ src/components/container/Home.jsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { showCost, loadCost } from "../../redux/actions/routeActions";
 import { useDispatch } from "react-redux";
 import Form from "../presentational/Form";
+import Map from "../presentational/Map";
 import Spinner from "../presentational/Spinner";
 
 const Home = () => {
@@ -39,6 +40,22 @@ const Home = () => {
     dispatch(loadCost(option, vehicle, distance, origin, destination, costRatio));
   };
 
+  const googleMapsApiKey = "AIzaSyBkrYRsEcxYmWtNcs5DlaNV76DOMHlpUfA";
+
+  const {
+    loadingElement,
+    containerElement,
+    mapElement,
+    defaultCenter,
+    defaultZoom
+  } = {}
+
+  const places = [
+    {latitude: 25.8103146,longitude: -80.1751609},
+    {latitude: 27.9947147,longitude: -82.5943645},
+    {latitude: 28.4813018,longitude: -81.4387899}
+  ]
+
   return (
     <main className="home">
       <h1>This is a Route Cost Calculator for Hedyla</h1>
@@ -73,6 +90,21 @@ const Home = () => {
       ) : (
         <></>
       )}
+    <section className="map">
+    <Map
+      googleMapURL={
+        'https://maps.googleapis.com/maps/api/js?key=' +
+        googleMapsApiKey +
+        '&libraries=geometry,drawing,places'
+      }
+      markers={places}
+      loadingElement={loadingElement || <div style={{height: `100%`}}/>}
+      containerElement={containerElement || <div style={{height: "80vh"}}/>}
+      mapElement={mapElement || <div style={{height: `100%`}}/>}
+      defaultCenter={defaultCenter || {lat: 25.798939, lng: -80.291409}}
+      defaultZoom={defaultZoom || 11}
+    />
+    </section>
     </main>
   );
 };
