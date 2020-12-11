7ecab5ffe57578b883a77697615049087ea1971a
Dani Alcala <daniamcode@gmail.com>
2020-12-11 17:11:46 +0100

    refactor Home, creating Form presentational component

--- src/components/container/Home.jsx
+++ src/components/container/Home.jsx
@@ -3,16 +3,15 @@ import "../styles/Home.css";
 import { useSelector } from "react-redux";
 import { loadCost } from "../../redux/actions/routeActions";
 import { useDispatch } from "react-redux";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
+import Form from "../presentational/Form";
 
 const Home = () => {
   let dispatch = useDispatch();
-
   let [distance, setDistance] = useState("");
   let [costRatio, setCostRatio] = useState("");
+
   const cost = useSelector((state) => state.routeCalculatorReducer.loadCost);
-  console.log(cost)
+  console.log(cost);
 
   const onFieldChange = (value, setValue) => {
     setValue(value);
@@ -28,47 +27,18 @@ const Home = () => {
     <main className="home">
       <h1>This is a Route Calculator for Hedyla</h1>
       <section className="home__form-container">
-        <form className="home__form" onSubmit={handleSubmit}>
-          <div className="home__form-title">
-            Introduce the data and calculate the route!
-          </div>
-          <div className="home__form-inner-container">
-            <TextField
-              id="distance"
-              variant="filled"
-              className="home__form-input"
-              placeholder="Introduce the distance in km"
-              name="distance"
-              required
-              value={distance}
-              onChange={(event) =>
-                onFieldChange(event.target.value, setDistance)
-              }
-            />
-            <TextField
-              id="costRatio"
-              variant="filled"
-              className="home__form-input"
-              placeholder="Introduce the cost in €/km"
-              name="costRatio"
-              required
-              value={costRatio}
-              onChange={(event) =>
-                onFieldChange(event.target.value, setCostRatio)
-              }
-            />
-            <Button
-              variant="contained"
-              color="primary"
-              className="home__form-button"
-              type="submit"
-            >
-              Calculate cost of the route
-            </Button>
-          </div>
-        </form>
+        <Form
+          distance={distance}
+          setDistance={setDistance}
+          costRatio={costRatio}
+          setCostRatio={setCostRatio}
+          onFieldChange={onFieldChange}
+          handleSubmit={handleSubmit}
+        />
       </section>
-        <h3 className="home__calculatated-message">The resulting cost is {cost} euros.</h3>
+      <h3 className="home__calculatated-message">
+        The resulting cost is {cost} euros.
+      </h3>
     </main>
   );
 };
---
+++ src/components/presentational/Form.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+const Form = ({distance, setDistance, costRatio, setCostRatio, onFieldChange, handleSubmit}) => {
+    return (
+        <form className="home__form" onSubmit={handleSubmit}>
+          <div className="home__form-title">
+            Introduce the data and calculate the route!
+          </div>
+          <div className="home__form-inner-container">
+            <TextField
+              id="distance"
+              variant="filled"
+              className="home__form-input"
+              placeholder="Introduce the distance in km"
+              name="distance"
+              required
+              value={distance}
+              onChange={(event) =>
+                onFieldChange(event.target.value, setDistance)
+              }
+            />
+            <TextField
+              id="costRatio"
+              variant="filled"
+              className="home__form-input"
+              placeholder="Introduce the cost in €/km"
+              name="costRatio"
+              required
+              value={costRatio}
+              onChange={(event) =>
+                onFieldChange(event.target.value, setCostRatio)
+              }
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              className="home__form-button"
+              type="submit"
+            >
+              Calculate cost of the route
+            </Button>
+          </div>
+        </form>
+    )
+}
+
+export default Form
