ebe064a887b1585dc6a15de48e974a7f50b444b9
Dani Alcala <daniamcode@gmail.com>
2020-12-12 01:14:34 +0100

    add first version of the map, clear stuff that I won't use

--- src/components/container/Home.jsx
+++ src/components/container/Home.jsx
@@ -6,6 +6,8 @@ import { useDispatch } from "react-redux";
 import Form from "../presentational/Form";
 import Map from "../presentational/Map";
 import Spinner from "../presentational/Spinner";
+import { withScriptjs, withGoogleMap } from "react-google-maps";
+import { googleMapsApiKey } from "../../data/constants";
 
 const Home = () => {
   let dispatch = useDispatch();
@@ -37,24 +39,12 @@ const Home = () => {
     event.preventDefault();
     event.target.reset();
     dispatch(showCost());
-    dispatch(loadCost(option, vehicle, distance, origin, destination, costRatio));
+    dispatch(
+      loadCost(option, vehicle, distance, origin, destination, costRatio)
+    );
   };
 
-  const googleMapsApiKey = "AIzaSyBkrYRsEcxYmWtNcs5DlaNV76DOMHlpUfA";
-
-  const {
-    loadingElement,
-    containerElement,
-    mapElement,
-    defaultCenter,
-    defaultZoom
-  } = {}
-
-  const places = [
-    {latitude: 25.8103146,longitude: -80.1751609},
-    {latitude: 27.9947147,longitude: -82.5943645},
-    {latitude: 28.4813018,longitude: -81.4387899}
-  ]
+  const WrappedMap = withScriptjs(withGoogleMap(Map));
 
   return (
     <main className="home">
@@ -90,21 +80,14 @@ const Home = () => {
       ) : (
         <></>
       )}
-    <section className="map">
-    <Map
-      googleMapURL={
-        'https://maps.googleapis.com/maps/api/js?key=' +
-        googleMapsApiKey +
-        '&libraries=geometry,drawing,places'
-      }
-      markers={places}
-      loadingElement={loadingElement || <div style={{height: `100%`}}/>}
-      containerElement={containerElement || <div style={{height: "80vh"}}/>}
-      mapElement={mapElement || <div style={{height: `100%`}}/>}
-      defaultCenter={defaultCenter || {lat: 25.798939, lng: -80.291409}}
-      defaultZoom={defaultZoom || 11}
-    />
-    </section>
+      <section className="map">
+        <WrappedMap
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.expkey&libraries=geometry,drawing,places&key=${googleMapsApiKey}`}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: "80vh" }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </section>
     </main>
   );
 };
--- src/data/constants.js
+++ src/data/constants.js
@@ -3,3 +3,4 @@ export const option2 = 'Via Origin and Destination'
 export const carFee = 0
 export const vanFee = 0.25
 export const truckFee = 0.5
+export const googleMapsApiKey = "AIzaSyBkrYRsEcxYmWtNcs5DlaNV76DOMHlpUfA";
